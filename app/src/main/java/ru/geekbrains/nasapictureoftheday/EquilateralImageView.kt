c773cbaa8205789861129c702494eb251cc84e23
kalinovdv <kdv_ul@rambler.ru>
2022-02-09 23:25:13 +0400

    Добавил класс для отображения равносторонних фотографий.

---
+++ app/src/main/java/ru/geekbrains/nasapictureoftheday/EquilateralImageView.kt
@@ -0,0 +1,15 @@
+package ru.geekbrains.nasapictureoftheday
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class EquilateralImageView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttribute: Int = 0
+) : AppCompatImageView(context, attributeSet, defStyleAttribute) {
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+}
