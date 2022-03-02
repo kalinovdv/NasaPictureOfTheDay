3d8e05ae211f6d79988f8eb15a50dd012d885edf
kalinovdv <kdv_ul@rambler.ru>
2022-03-03 00:16:16 +0400

    Добавил на main_fragment чипы для выбора картинки: позавчера, вчера, сегодня.

--- app/src/main/java/ru/geekbrains/nasapictureoftheday/picture/PictureOfTheDayRetrofitImpl.kt
+++ app/src/main/java/ru/geekbrains/nasapictureoftheday/picture/PictureOfTheDayRetrofitImpl.kt
@@ -1,50 +1,8 @@
 package ru.geekbrains.nasapictureoftheday.picture
 
-//import com.google.gson.GsonBuilder
-//import okhttp3.Interceptor
-//import okhttp3.OkHttpClient
-//import okhttp3.logging.HttpLoggingInterceptor
-//import retrofit2.Retrofit
-//import retrofit2.converter.gson.GsonConverterFactory
-//import java.io.IOException
-//import kotlin.jvm.Throws
-//
-//class PictureOfTheDayRetrofitImpl {
-//    private val baseUrl = "https://api.nasa.gov/"
-//
-//    fun getRetrofitImpl(): PictureOfTheDayAPI {
-//        val podRetrofit = Retrofit.Builder().baseUrl(baseUrl)
-//            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
-//            .client(createOkHttpClient(PODInterceptor()))
-//            .build()
-//
-//        return podRetrofit.create(PictureOfTheDayAPI::class.java)
-//    }
-//
-//    private fun createOkHttpClient(interceptor: Interceptor): OkHttpClient {
-//        val httpClient = OkHttpClient.Builder()
-//        httpClient.addInterceptor(interceptor)
-//        httpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-//
-//        return httpClient.build()
-//    }
-//
-//    inner class PODInterceptor : Interceptor {
-//
-//        @Throws(IOException::class)
-//        override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
-//            return chain.proceed(chain.request())
-//        }
-//    }
-//}
 import com.google.gson.GsonBuilder
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.io.IOException
-import kotlin.jvm.Throws
 
 class PictureOfTheDayRetrofitImpl {
 
@@ -54,23 +12,7 @@ class PictureOfTheDayRetrofitImpl {
         val podRetrofit = Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
-            //.client(createOkHttpClient(PODInterceptor()))
             .build()
         return podRetrofit.create(PictureOfTheDayAPI::class.java)
     }
-
-    /*private fun createOkHttpClient(interceptor: Interceptor): OkHttpClient {
-        val httpClient = OkHttpClient.Builder()
-        httpClient.addInterceptor(interceptor)
-        httpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-        return httpClient.build()
-    }
-
-    inner class PODInterceptor : Interceptor {
-
-        @Throws(IOException::class)
-        override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
-            return chain.proceed(chain.request())
-        }
-    }*/
 }
